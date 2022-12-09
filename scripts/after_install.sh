fa5b5cd5750f411cb9183afa9a85d7a73df9062a
YongjuLee <ivan.yongju.lee@gmail.com>
2022-12-08 18:27:57 -0800

    updated appspec.yml

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,7 @@
 echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+cd /home/ec2-user/clothes-api-clcd >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
 npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
